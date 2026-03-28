2fa2f78fd55bbf42282e780e46aff0e628be9c75
sak96 <26397224+sak96@users.noreply.github.com>
2026-03-28 15:24:19 +0530

    Fix inset

--- src/composables/useInset.ts
+++ src/composables/useInset.ts
@@ -1,24 +1,57 @@
-import { invokeParseLogError } from './useTauri'
+import { invoke, addPluginListener, PluginListener } from '@tauri-apps/api/core'
+
+interface GetInsetResponse {
+  inset: number
+}
+
+export async function getTopInset(): Promise<GetInsetResponse | null> {
+  return await invoke<GetInsetResponse>('plugin:safe-area-insets-css|get_top_inset', {
+    payload: {},
+  });
+}
+
+export async function getBottomInset(): Promise<GetInsetResponse | null> {
+  return await invoke<GetInsetResponse>('plugin:safe-area-insets-css|get_bottom_inset', {
+    payload: {},
+  });
+}
+
+export async function onKeyboardShown(
+  handler: () => void
+): Promise<PluginListener> {
+  return await addPluginListener(
+    'safe-area-insets-css',
+    'keyboard_shown',
+    handler
+  );
+}
+
+export async function onKeyboardHidden(
+  handler: () => void
+): Promise<PluginListener> {
+  return await addPluginListener(
+    'safe-area-insets-css',
+    'keyboard_hidden',
+    handler
+  );
+}
 
 export async function setInset(): Promise<void> {
   try {
-    const topResponse = await invokeParseLogError<{ position: { y: number } }>('plugin:window|outer_position', {})
-    const bottomResponse = await invokeParseLogError<{ size: { height: number } }>('plugin:window|outer_size', {})
-    
-    if (topResponse) {
-      document.documentElement.style.setProperty(
-        'safe-area-inset-top',
-        `${topResponse.position.y}px`
-      )
+    const topInset = await getTopInset();
+    const bottomInset = await getBottomInset();
+    if (topInset) {
+      document.documentElement.style.setProperty('--safe-area-inset-top', `${topInset?.inset}px`);
     }
-    if (bottomResponse) {
-      const height = bottomResponse.size.height
-      const top = topResponse?.position.y ?? 0
-      document.documentElement.style.setProperty(
-        'safe-area-inset-bottom',
-        `${Math.max(window.innerHeight - height - top, 0)}px`
-      )
+    if (bottomInset) {
+      document.documentElement.style.setProperty('--safe-area-inset-bottom', `${bottomInset?.inset}px`);
     }
+    onKeyboardShown(() => {
+      document.documentElement.style.setProperty('--safe-area-inset-bottom', `0px`);
+    });
+    onKeyboardHidden(() => {
+      document.documentElement.style.setProperty('--safe-area-inset-bottom', `${bottomInset?.inset}px`);
+    });
   } catch {
     if (CSS.supports('env(safe-area-inset-top)')) {
       const computedStyle = getComputedStyle(document.documentElement)
