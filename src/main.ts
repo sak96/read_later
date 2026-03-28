bac6ca293bf6b377cdb556a4ccd6c120b11e18c3
sak96 <26397224+sak96@users.noreply.github.com>
2026-03-28 23:02:41 +0530

    remove inset part as await was not working propelry before

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { setInset } from './composables/useInset'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
-
-setInset()
