b9e74fdbf3fc24d1333af1e55be6d3beddb3e7fa
sak96 <26397224+sak96@users.noreply.github.com>
2026-03-28 14:10:17 +0530

    Fix vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+const host = process.env.TAURI_DEV_HOST
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,16 +13,27 @@ export default defineConfig({
   },
   clearScreen: false,
   server: {
-    port: 8080,
+    port: 5173,
     strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
     watch: {
       ignored: ['**/src-tauri/**'],
     },
   },
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
-    target: 'esnext',
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    sourcemap: !!process.env.TAURI_DEBUG,
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
 })
