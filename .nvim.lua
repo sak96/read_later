478d68ce8f9ec4b4b6e131af60105d55a2cfb273
sak96 <26397224+sak96@users.noreply.github.com>
2026-03-29 17:30:11 +0530

    Add linter configuation

--- .nvim.lua
+++ .nvim.lua
@@ -1,15 +1,52 @@
-local tsserver_filetypes = { "typescript", "vue" }
+---@diagnostic disable: undefined-global
+local vim = vim
+
+local vue_filetypes = { "typescript", "vue" }
 local vue_plugin = {
 	name = "@vue/typescript-plugin",
 	location = "node_modules/.bin/vue-language-server",
 	languages = { "vue" },
 	configNamespace = "typescript",
 }
+
+-- enable language server
 vim.lsp.config("ts_ls", {
 	cmd = { "node_modules/.bin/typescript-language-server", "--stdio" },
 	init_options = {
 		plugins = { vue_plugin },
 	},
-	filetypes = { "typescript", "vue" },
+	filetypes = vue_filetypes,
 })
 vim.lsp.enable("ts_ls")
+
+-- enable linter
+local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+null_ls.register(helpers.make_builtin({
+	name = "eslint",
+	method = methods.internal.FORMATTING,
+	filetypes = vue_filetypes,
+	factory = helpers.generator_factory,
+	generator_opts = {
+		command = "node_modules/.bin/eslint",
+		args = { "--fix-dry-run", "--format", "json", "--stdin", "--stdin-filename", "$FILENAME" },
+		to_stdin = true,
+		format = "json",
+		on_output = function(params)
+			local parsed = params.output[1]
+			return parsed
+				and parsed.output
+				and {
+					{
+						row = 1,
+						col = 1,
+						end_row = #vim.split(parsed.output, "\n") + 1,
+						end_col = 1,
+						text = parsed.output,
+					},
+				}
+		end,
+		check_exit_code = { 0, 1 },
+	},
+}))
