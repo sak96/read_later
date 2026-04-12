f3a3f78ac2e29df304179b5c38e85fe32123c8c2
sak96 <26397224+sak96@users.noreply.github.com>
2026-04-12 20:00:41 +0530

    Only register media listener if mobile

--- src/composables/useMediaSession.ts
+++ src/composables/useMediaSession.ts
@@ -1,10 +1,12 @@
 import { addPluginListener, PluginListener } from '@tauri-apps/api/core'
+import { platform } from '@tauri-apps/plugin-os'
 
 /*
  * Source: https://docs.rs/crate/tauri-plugin-media-session/0.2.4/source/guest-js/
  */
 
 export type MediaAction = 'play' | 'pause' | 'stop' | 'next' | 'previous' | 'seek'
+const currentPlatform: string = platform()
 
 export interface MediaActionEvent {
   action: MediaAction
@@ -13,10 +15,13 @@ export interface MediaActionEvent {
 
 export async function onAction(
   handler: (event: MediaActionEvent) => void,
-): Promise<PluginListener> {
-  return addPluginListener<MediaActionEvent>(
-    'media-session',
-    'media_action',
-    handler,
-  )
+): Promise<PluginListener | null> {
+  if (currentPlatform === 'android' || currentPlatform == 'ios') {
+    return addPluginListener<MediaActionEvent>(
+      'media-session',
+      'media_action',
+      handler,
+    )
+  }
+  return null
 }
