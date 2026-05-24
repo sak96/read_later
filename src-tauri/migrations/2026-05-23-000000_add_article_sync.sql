f79ab91d2e293470a7413885d3223494611f438a
sak96 <26397224+sak96@users.noreply.github.com>
2026-05-24 18:11:24 +0530

    Fix issue with migration

--- src-tauri/migrations/2026-05-23-000000_add_article_sync.sql
+++ src-tauri/migrations/2026-05-23-000000_add_article_sync.sql
@@ -1,6 +1,6 @@
 -- add updated_at column
 ALTER TABLE articles
-ADD COLUMN updated_at TEXT NOT NULL DEFAULT (datetime('now'));
+ADD COLUMN updated_at TEXT NOT NULL DEFAULT '1970-01-01 00:00:10';
 
 -- add is_deleted column
 ALTER TABLE articles
